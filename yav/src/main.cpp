968c839e6c894e00fb818f8de874ff639fcc3fc5
Erwan MATHIEU <erwan.mathieu@ultimaker.com>
2026-05-26 21:42:33 +0200

    Add code formatting

--- yav/src/main.cpp
+++ yav/src/main.cpp
@@ -1,27 +1,27 @@
 #include <cxxopts.hpp>
-
 #include <iostream>
 
 #include "yav/yav.hpp"
 
-int main(int argc, char** argv) {
-  cxxopts::Options options("yav", "Minimal CLI for libyav");
-  options.add_options()
-      ("v,version", "Print libyav version")
-      ("h,help", "Print help");
+int main(int argc, char** argv)
+{
+    cxxopts::Options options("yav", "Minimal CLI for libyav");
+    options.add_options()("v,version", "Print libyav version")("h,help", "Print help");
 
-  const auto result = options.parse(argc, argv);
+    const auto result = options.parse(argc, argv);
 
-  if (result.count("help") > 0U) {
-    std::cout << options.help() << '\n';
+    if (result.count("help") > 0U)
+    {
+        std::cout << options.help() << '\n';
+        return 0;
+    }
+
+    if (result.count("version") > 0U)
+    {
+        std::cout << yav::version() << '\n';
+        return 0;
+    }
+
+    std::cout << "Run with --help to list options.\n";
     return 0;
-  }
-
-  if (result.count("version") > 0U) {
-    std::cout << yav::version() << '\n';
-    return 0;
-  }
-
-  std::cout << "Run with --help to list options.\n";
-  return 0;
 }
--- yavui/src/main.cpp
+++ yavui/src/main.cpp
@@ -1,16 +1,17 @@
 #include <QApplication>
 #include <QLabel>
-
 #include <string>
 
 #include "yav/yav.hpp"
 
-int main(int argc, char** argv) {
-  QApplication app(argc, argv);
 
-  QLabel label(QString::fromStdString("yavui using " + yav::version()));
-  label.resize(320, 80);
-  label.show();
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
 
-  return app.exec();
+    QLabel label(QString::fromStdString("yavui using " + yav::version()));
+    label.resize(320, 80);
+    label.show();
+
+    return app.exec();
 }
