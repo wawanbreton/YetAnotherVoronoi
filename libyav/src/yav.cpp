26ca4779cb25782f7c6d3404bbe11e99b2fd2557
Erwan MATHIEU <wawanbreton@gmail.com>
2026-05-26 21:42:33 +0200

    Add code formatting

--- libyav/src/yav.cpp
+++ libyav/src/yav.cpp
@@ -1,7 +1,11 @@
 #include "yav/yav.hpp"
 
-namespace yav {
-std::string version() {
-  return "libyav 0.1.0";
+namespace yav
+{
+
+std::string version()
+{
+    return "libyav 0.1.0";
 }
-}
+
+} // namespace yav
