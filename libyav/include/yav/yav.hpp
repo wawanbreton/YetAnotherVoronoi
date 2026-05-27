37a5bf9eba48612c27c409b63813bc9410c763ff
Erwan MATHIEU <wawanbreton@gmail.com>
2026-05-27 08:41:41 +0200

    Add copyright notices

--- libyav/include/yav/yav.hpp
+++ libyav/include/yav/yav.hpp
@@ -1,7 +1,13 @@
+// Copyright (C) 2026 Erwan MATHIEU
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 #pragma once
 
 #include <string>
 
-namespace yav {
+namespace yav
+{
+
 std::string version();
+
 }
--- libyav/src/yav.cpp
+++ libyav/src/yav.cpp
@@ -1,3 +1,6 @@
+// Copyright (C) 2026 Erwan MATHIEU
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 #include "yav/yav.hpp"
 
 namespace yav
