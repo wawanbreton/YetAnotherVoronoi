6f7375f5627d94835aea8111a071c9ba35341a50
Erwan MATHIEU <wawanbreton@gmail.com>
2026-06-06 23:13:54 +0200

    Remove unnecessary classes/code

--- libyav/include/yav/voronoi/Diagram.h
+++ libyav/include/yav/voronoi/Diagram.h
@@ -9,29 +9,14 @@
 namespace yav
 {
 
-namespace space
-{
-class Primitive;
-}
-
 namespace voronoi
 {
-
-class Cell;
 
 /** Full analytical Voronoi diagram with all generated cells and patches. */
 class Diagram
 {
 public:
     Diagram();
-
-    void addCell(const std::shared_ptr<space::Primitive>& primitive, const std::shared_ptr<Cell>& cell);
-
-    std::shared_ptr<Cell> findCell(const std::shared_ptr<space::Primitive>& primitive) const;
-    const std::unordered_map<std::shared_ptr<space::Primitive>, std::shared_ptr<Cell>>& cells() const;
-
-private:
-    std::unordered_map<std::shared_ptr<space::Primitive>, std::shared_ptr<Cell>> cells_;
 };
 
 } // namespace voronoi
--- libyav/src/voronoi/Diagram.cpp
+++ libyav/src/voronoi/Diagram.cpp
@@ -11,29 +11,4 @@ namespace yav::voronoi
 
 Diagram::Diagram() = default;
 
-void Diagram::addCell(const std::shared_ptr<space::Primitive>& primitive, const std::shared_ptr<Cell>& cell)
-{
-    if (! cells_.insert({ primitive, cell }).second)
-    {
-        spdlog::warn("Primitive has been inserted already");
-    }
-}
-
-std::shared_ptr<Cell> Diagram::findCell(const std::shared_ptr<space::Primitive>& primitive) const
-{
-    const auto iterator = cells_.find(primitive);
-
-    if (iterator == cells_.end())
-    {
-        return nullptr;
-    }
-
-    return iterator->second;
-}
-
-const std::unordered_map<std::shared_ptr<space::Primitive>, std::shared_ptr<Cell>>& Diagram::cells() const
-{
-    return cells_;
-}
-
 } // namespace yav::voronoi
